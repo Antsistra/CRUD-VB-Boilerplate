70274e8f1095470aeb53d131ab9c27e4be6ac8c6
Hadinmd <hadinurmuhamad8@gmail.com>
2023-11-06 22:41:44 +0700

    init

--- CRUDVB/Form1.Designer.vb
+++ CRUDVB/Form1.Designer.vb
@@ -22,10 +22,141 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        DataGridView1 = New DataGridView()
+        ComboBox1 = New ComboBox()
+        ComboBox2 = New ComboBox()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(170, 318)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "Input "
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(170, 51)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(210, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(170, 95)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(210, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(170, 139)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(210, 23)
+        TextBox3.TabIndex = 6
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(170, 184)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(210, 23)
+        TextBox4.TabIndex = 7
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(305, 318)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 34)
+        Button2.TabIndex = 8
+        Button2.Text = "Edit"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(170, 374)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 34)
+        Button3.TabIndex = 9
+        Button3.Text = "Delete"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(305, 374)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 34)
+        Button4.TabIndex = 10
+        Button4.Text = "Close"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(481, 51)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowTemplate.Height = 25
+        DataGridView1.Size = New Size(590, 391)
+        DataGridView1.TabIndex = 11
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(170, 227)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(210, 23)
+        ComboBox1.TabIndex = 12
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Location = New Point(173, 270)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(207, 23)
+        ComboBox2.TabIndex = 13
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1108, 502)
+        Controls.Add(ComboBox2)
+        Controls.Add(ComboBox1)
+        Controls.Add(DataGridView1)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = " "
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
 End Class
--- CRUDVB/Form1.vb
+++ CRUDVB/Form1.vb
@@ -1,3 +1,81 @@
-﻿Public Class Form1
+﻿Imports System.Data.Odbc
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
+Public Class Form1
+    Dim Conn As OdbcConnection
+    Dim cmd As OdbcCommand
+    Dim Ds As DataSet
+    Dim Da As OdbcDataAdapter
+    Dim Rd As OdbcDataReader
+    Dim MyDB As String
+
+    Sub Connection()
+        MyDB = "Driver={MySQL ODBC 8.1 ANSI Driver};Database=dbnama;server=localhost;uid=root"
+        Conn = New OdbcConnection(MyDB)
+        If Conn.State = ConnectionState.Closed Then Conn.Open()
+    End Sub
+
+    Sub KondisiAwal()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        ComboBox1.Text = ""
+        ComboBox2.Text = ""
+
+        Call Connection()
+        Da = New OdbcDataAdapter("select * from namatabel", Conn)
+        Ds = New DataSet
+        Da.Fill(Ds, "namatabel")
+        DataGridView1.DataSource = Ds.Tables("namatabel")
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call KondisiAwal()
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call Connection()
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Or ComboBox1.Text = "" Or ComboBox2.Text = "" Then
+            MessageBox.Show("Silahkan Mengisi data Secara Lengkap")
+        Else
+            Dim InputData As String = " Insert into namatabel value ('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "','" & TextBox4.Text & "','" & ComboBox1.Text & "','" & ComboBox2.Text & "')"
+            cmd = New OdbcCommand(InputData, Conn)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Input Data Berhasil")
+            Call KondisiAwal()
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        End
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Call Connection()
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Or ComboBox1.Text = "" Or ComboBox2.Text = "" Then
+            MessageBox.Show("Silahkan Mengisi data Secara Lengkap")
+        Else
+            Dim DeleteData As String = "delete from namatabel where key='" & TextBox1.Text & "'"
+            cmd = New OdbcCommand(DeleteData, Conn)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Hapus Data Berhasil")
+            Call KondisiAwal()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Or ComboBox1.Text = "" Or ComboBox2.Text = "" Then
+            MessageBox.Show("Silahkan Mengisi data Secara Lengkap")
+        Else
+            Call Connection()
+            Dim EditData As String = "update namatabel Set nama='" & TextBox2.Text & "',jekel='" & ComboBox1.Text & "',prodi='" & ComboBox2.Text & "',alamat='" & TextBox3.Text & "',nohp='" & TextBox4.Text & "' where nim='" & TextBox1.Text & "'"
+            cmd = New OdbcCommand(EditData, Conn)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Edit Data Berhasil")
+            Call KondisiAwal()
+        End If
+
+    End Sub
 End Class
